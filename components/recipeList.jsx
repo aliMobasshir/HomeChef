fbb127ce519ded711d9dcf1435b8eb20ffe27e2b
aliMobasshir <mobasshir0001@gmail.com>
2024-11-15 11:35:17 +0530

    made today's top picks responsive

--- components/recipeList.jsx
+++ components/recipeList.jsx
@@ -4,7 +4,7 @@ import arrowIcon from './right-arrow.png';
 
 
 
-const apiKey = 'af3ad633e574425c90e2c0ef4a4fefc0'; //af3ad633e574425c90e2c0ef4a4fefc0 //3544e0a87f98468883e9169172546ac1
+const apiKey = '3544e0a87f98468883e9169172546ac1'; //af3ad633e574425c90e2c0ef4a4fefc0 //3544e0a87f98468883e9169172546ac1 0d0e212f1a904e9cb772072f49167a4b
 const endpoint = `https://api.spoonacular.com/recipes/random?apiKey=${apiKey}&number=6`;
 
 function RecipeList() {
