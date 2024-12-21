8c179f5fa13b6a21444193fd8a9e1d856d85dff7
aliMobasshir <mobasshir0001@gmail.com>
2024-12-21 13:49:26 +0530

    dist error

---
+++ components/KnowAbtDiets.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const KnowAbtDiets = () => {
+    return (
+        <div>
+        <h1>Gluten Free</h1>
+        <p>
+        Eliminating gluten means avoiding wheat, barley, rye, and other gluten-containing grains and foods made from them.
+        </p>
+
+        <h1>Ketogenic</h1>
+        <p>The keto diet is based more on the ratio of fat, protein, and carbs in the diet rather than specific ingredients. Generally speaking, high fat, protein-rich foods are acceptable and high carbohydrate foods are not. The formula we use is 55-80% fat content, 15-35% protein content, and under 10% of carbohydrates.</p>
+
+        </div>
+
+    );
+}
+
+export default KnowAbtDiets;
